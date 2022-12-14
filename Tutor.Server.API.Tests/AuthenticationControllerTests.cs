e947a6af70b0c15702e2b598cde566dff2e8c249
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-12-14 21:22:02 +0100

    add test for valid registration fails as functionality not implemented yet

---
+++ Tutor.Server.API.Tests/AuthenticationControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tutor.Server.Infrastructure.Database;
+
+namespace Tutor.Server.API.Tests;
+
+public class AuthenticationControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public AuthenticationControllerTests(WebApplicationFactory<Program> factory)
+	{
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextService = services.FirstOrDefault(s => s.ServiceType == typeof(TutorDbContext));
+                services.Remove(dbContextService!);
+                var optionsBuilder = new DbContextOptionsBuilder();
+                optionsBuilder.UseInMemoryDatabase("TutorDb");
+                var context = new TutorDbContext(optionsBuilder.Options);
+                services.AddSingleton(context);
+            });
+        });
+	}
+
+    [Fact]
+    public async Task Register_ForValidModel_ReturnsOkStatusCode()
+    {
+        var dto = new RegisterUserDto("John", "Smith", "User", "email@email.com", "!Password123", "!Password132");
+        var client = _factory.CreateClient();
+        var result = await client.PostAsJsonAsync("api/authentication/register", dto);
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
---
+++ Tutor.Server.API.Tests/Usings.cs
@@ -0,0 +1,5 @@
+global using Xunit;
+global using FluentAssertions;
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using System.Net;
+global using Tutor.Shared.Dtos;
