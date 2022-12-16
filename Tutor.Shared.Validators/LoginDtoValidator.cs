701fa12e236c1dab226baa34f0f95f1a79458b4d
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-12-16 19:31:37 +0100

    validation rule for password

--- Tutor.Shared.Validators/LoginDtoValidator.cs
+++ Tutor.Shared.Validators/LoginDtoValidator.cs
@@ -5,4 +5,10 @@ namespace Tutor.Shared.Validators;
 
 internal class LoginDtoValidator : AbstractValidator<LoginDto>
 {
+	public LoginDtoValidator()
+	{
+		RuleFor(l => l.Password)
+			.NotEmpty()
+			.WithMessage("Password must not be empty.");
+	}
 }
