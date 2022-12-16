c59eaf6caf02991da701342aafe8ed5c634535c2
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-12-16 19:26:35 +0100

    add login dto validator with test for valid model -  passes

---
+++ Tutor.Shared.Validators.Tests/LoginDtoValidatorTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Tutor.Shared.Validators.Tests;
+
+public class LoginDtoValidatorTests 
+{
+	private readonly LoginDtoValidator _validator;
+
+	public LoginDtoValidatorTests()
+	{
+		_validator = new();
+	}
+
+	[Fact]
+	public void Validate_ForValidModel_DOntHaveAnyErrors()
+	{
+		var model = new LoginDto("email", "password");
+		var result = _validator.TestValidate(model);
+		result.ShouldNotHaveAnyValidationErrors();
+	}
+}
---
+++ Tutor.Shared.Validators/LoginDtoValidator.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+using Tutor.Shared.Dtos;
+
+namespace Tutor.Shared.Validators;
+
+internal class LoginDtoValidator : AbstractValidator<LoginDto>
+{
+}
