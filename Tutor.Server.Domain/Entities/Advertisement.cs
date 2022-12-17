17c5bd9f4dc1d930024fe32f0ac218829366c008
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-12-17 15:58:13 +0100

    add Advertisement entity

---
+++ Tutor.Server.Domain/Entities/Advertisement.cs
@@ -0,0 +1,17 @@
+﻿using Tutor.Shared.Enums;
+
+namespace Tutor.Server.Domain.Entities;
+
+public class Advertisement
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime LastModificationDate { get; set; }
+    public EducationLevels Levels { get; set; }
+    public Subject Subject { get; set; }
+    public decimal PricePerHour { get; set; }
+    public Guid CreatedById { get; set; }
+    public User CreatedBy { get; set; }
+}
---
+++ Tutor.Shared/Enums/EducationLevels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Shared.Enums
+{
+    internal class EducationLevel
+    {
+    }
+}
