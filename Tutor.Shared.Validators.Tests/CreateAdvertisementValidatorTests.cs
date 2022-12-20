ef6fb53a47068f0811f4a2db156cc5195ae046dc
Pawel Felcyn <pawel.felcyn@onet.pl>
2022-12-20 19:39:26 +0100

    addd tests for title - fail

--- Tutor.Shared.Validators.Tests/CreateAdvertisementValidatorTests.cs
+++ Tutor.Shared.Validators.Tests/CreateAdvertisementValidatorTests.cs
@@ -18,4 +18,15 @@ public class CreateAdvertisementValidatorTests
 		var result = _validator.TestValidate(model);
 		result.ShouldNotHaveAnyValidationErrors();
 	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData("sadfdsfhsdkjfhkdjshfdjshfjhsdkjfhkjsdhfkjshdfsdhfjsdhfkdjsfhkdjshfkjhsdfjhdskfhkdjshfkdjshfsdkjfhkdjshfkdjshfkdjshfksdhkdjfhksfkjsfhkdsjfhkdjshfkdhsf")]
+	public void Validate_ForInvalidTitle_ReturnsProperError(string title)
+	{
+		var model = new CreateAdvertisementDto(title, "Description", EducationLevels.Primary, Subject.English, 40);
+        var result = _validator.TestValidate(model);
+		result.ShouldHaveValidationErrorFor(m => m.Title);
+    }
 }
