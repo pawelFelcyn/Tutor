6662f3799bdae4bf2fba4ce85e7bf786a2804500
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-01-03 21:50:45 +0100

    fix title type - from int to string

--- Tutor.Shared.Dtos/AdvertisementDto.cs
+++ Tutor.Shared.Dtos/AdvertisementDto.cs
@@ -5,7 +5,7 @@ namespace Tutor.Shared.Dtos;
 public record AdvertisementDto
 {
     public Guid Id { get; init; }
-    public int Title { get; init; }
+    public string Title { get; init; }
     public DateTime CreationDate { get; init; }
     public DateTime LastModificationDate { get; init;}
     public EducationLevels Levels { get; init; }
