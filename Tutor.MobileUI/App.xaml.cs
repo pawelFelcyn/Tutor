f70a7290fca4279b21907bf8bb4a40e1b3442f68
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-01-05 20:30:39 +0100

    create projects with ui and view models

---
+++ Tutor.MobileUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tutor.MobileUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
