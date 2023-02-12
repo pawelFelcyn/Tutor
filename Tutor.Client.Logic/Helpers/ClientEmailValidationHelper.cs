c3ed17df10236f909236283a3e03416ec3657bb9
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-02-12 19:43:43 +0100

    add client email validation helper

---
+++ Tutor.Client.Logic/Helpers/ClientEmailValidationHelper.cs
@@ -0,0 +1,8 @@
+﻿using Tutor.Shared.Helpers.Abstractions;
+
+namespace Tutor.Client.Logic.Helpers;
+
+internal class ClientEmailValidationHelper : IEmailValidationHelper
+{
+    public bool IsEmailTaken(string email) => false;
+}
