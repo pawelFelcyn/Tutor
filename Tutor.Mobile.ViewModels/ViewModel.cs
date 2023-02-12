9306820a2403f60bac06ea38bb740992f5336377
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-02-12 19:08:44 +0100

    create base class for view model

---
+++ Tutor.Mobile.ViewModels/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Tutor.Mobile.ViewModels;
+
+public partial class ViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _title;
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    private bool _isBusy;
+    public bool IsNotBusy => IsBusy;
+
+    private readonly object _isBusyLock = new();
+    protected bool CheckIsBusy()
+    {
+        lock (_isBusyLock)
+        {
+            if (IsBusy)
+            {
+                return true;
+            }
+
+            IsBusy = true;
+            return false;
+        }
+    }
+}
