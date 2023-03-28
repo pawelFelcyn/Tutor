2720ff41dd3e7754459536ff616605764815d364
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-03-28 22:24:13 +0200

    add setters to CreateAdvertisementDto

--- Tutor.Shared.Dtos/CreateAdvertisementDto.cs
+++ Tutor.Shared.Dtos/CreateAdvertisementDto.cs
@@ -2,9 +2,24 @@
 
 namespace Tutor.Shared.Dtos;
 
-public record CreateAdvertisementDto(string Title, string Description,
-    EducationLevels Levels, Guid SubjectId, decimal PricePerHour)
+public record CreateAdvertisementDto
 {
+    public CreateAdvertisementDto(string title, string description,
+    EducationLevels levels, Guid subjectId, decimal pricePerHour)
+    {
+        Title = title;
+        Description = description;
+        Levels = levels;
+        SubjectId = subjectId;
+        PricePerHour = pricePerHour;
+    }
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public EducationLevels Levels { get; set; }
+    public Guid SubjectId { get; set; }
+    public decimal PricePerHour { get; set; }
+
     public static CreateAdvertisementDto WithDefaultValues()
         => new(default, default, default, default, default);
 }
