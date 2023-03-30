d24a43d2480bb130b943a276f5a12fc4fe00dcd2
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-03-30 20:57:42 +0200

    disaply my advertisements in collection view

---
+++ Tutor.Client.Maui/Converters/NegationConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace Tutor.Client.Maui.Converters;
+
+public class NegationConverter : IValueConverter, IMarkupExtension
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => value is bool @bool ? !@bool : Binding.DoNothing;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => value is bool @bool ? !@bool : Binding.DoNothing;
+
+    public object ProvideValue(IServiceProvider serviceProvider) => this;
+}
