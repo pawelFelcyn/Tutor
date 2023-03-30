2706f1f41d2b2a360a602458c6a30f91725f081d
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-03-30 21:03:00 +0200

    add activity ind to MyAdvertisementsPage

---
+++ Tutor.Client.Maui/Converters/AndMultiConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Tutor.Client.Maui.Converters;
+
+public class AndMultiConverter : IMultiValueConverter, IMarkupExtension
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        => values?.All(v => v is bool b && b) ?? Binding.DoNothing;
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new InvalidOperationException("This converter can be uesd only with one way binding.");
+    }
+
+    public object ProvideValue(IServiceProvider serviceProvider) => this;
+}
