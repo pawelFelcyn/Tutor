b9fb376942880a09b42c9508948c03c95d7e81f0
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-04-01 14:12:05 +0200

    add logged in user context service

---
+++ Tutor.Client.Logic/Services/ILoggedUserContextService.cs
@@ -0,0 +1,8 @@
+﻿using Tutor.Shared.Dtos;
+
+namespace Tutor.Client.Logic.Services;
+
+public interface ILoggedUserContextService
+{
+    UserDetailsDto LoggedInUser { get; set; }
+}
---
+++ Tutor.MobileUI/Services/LoggedUserContextService.cs
@@ -0,0 +1,23 @@
+﻿using Tutor.Client.Logic.Services;
+using Tutor.Client.Logic.Static;
+using Tutor.Client.Maui.Extensions;
+using Tutor.Shared.Dtos;
+
+namespace Tutor.MobileUI.Services;
+
+internal class LoggedUserContextService : ILoggedUserContextService
+{
+    private readonly IPreferences _preferences;
+
+    public LoggedUserContextService(IPreferences preferences)
+    {
+        _preferences = preferences;
+    }
+
+    private UserDetailsDto _loggedUserDto;
+    public UserDetailsDto LoggedInUser 
+    { 
+        get => _loggedUserDto ??= _preferences.GetFromJson<UserDetailsDto>(PreferencesNames.LoggedUser);
+        set => _loggedUserDto = value; 
+    }
+}
