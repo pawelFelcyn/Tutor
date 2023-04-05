a6258afe2bacaa7c5382736f2e61b495f12986ca
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-04-05 19:57:43 +0200

    add extensions for finding first index meeting condition

---
+++ Tutor.Client.Logic.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using Tutor.Client.Logic.Extensions;
+
+namespace Tutor.Client.Logic.Tests.Extensions;
+
+public class EnumerableExtensionsTests
+{
+    public static IEnumerable<object[]> GetTestData_FirstIndex()
+    {
+        yield return new object[] { (int v) => v == 2, 1 };
+        yield return new object[] { (int v) => v > 0, 0 };
+        yield return new object[] { (int v) => v > 4, 4 };
+        yield return new object[] { (int v) => v % 5 == 0, 4 };
+    }
+
+
+    [Theory]
+    [MemberData(nameof(GetTestData_FirstIndex))]
+    public void FirstIndex_ForGivenCondition_ReturnsIndexOfFirstItemMeetingThatCondition(Func<int, bool> condition, int expected)
+    {
+        var source = new int[] { 1, 2, 3, 4, 5, 6 };
+        source.FirstIndex(condition).Should().Be(expected);
+    }
+
+    [Fact]
+    public void FirstIndex_ForSourceNull_ThrowsArgumentNullException()
+    {
+        int[] src = null!;
+        var action = () =>  src.FirstIndex(v => true);
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void FirstIndex_ForConditionNull_ThorowsArgumentNullException()
+    {
+        int[] src = new int[] { 1, 2, 3};
+        var action = () => src.FirstIndex(null);
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void FirstIndex_ForNoElementMeetingCondition_ReturnsMinutOne()
+    {
+        int[] src = new int[] { 1, 2, 3 };
+        src.FirstIndex(v => false).Should().Be(-1);
+    }
+}
---
+++ Tutor.Client.Logic/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Tutor.Client.Logic.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static int FirstIndex<T>(this IEnumerable<T> source, Func<T, bool> condition)
+    {
+        if (source is null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (condition is null)
+        {
+            throw new ArgumentNullException(nameof(condition));
+        }
+
+        int count = 0;
+        var enumerator = source.GetEnumerator();
+
+        while (enumerator.MoveNext()) 
+        {
+            if (condition(enumerator.Current))
+            {
+                return count;
+            }
+
+            count++;
+        }
+
+        return -1;
+    }
+}
