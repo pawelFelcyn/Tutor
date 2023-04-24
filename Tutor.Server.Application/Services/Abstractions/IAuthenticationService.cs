91515c899a77ff06505a7f13167292f3bb7bc2da
Pawel Felcyn <pawel.felcyn@onet.pl>
2023-04-24 19:59:52 +0200

    return user data on register

--- Tutor.Server.Application/Services/Abstractions/IAuthenticationService.cs
+++ Tutor.Server.Application/Services/Abstractions/IAuthenticationService.cs
@@ -4,7 +4,7 @@ namespace Tutor.Server.Application.Services.Abstractions;
 
 public interface IAuthenticationService
 {
-    Task RegisterAsync(RegisterUserDto dto);
+    Task<LoginResponseDto> RegisterAsync(RegisterUserDto dto);
     Task<LoginResponseDto> GetLoginResponseAsync(LoginDto dto);
     Task<string> RefreshTokenAsync();
 }
